87a3d6a2bff252e3e2a6757122481bbf9d04dde8
VI <aLeftXHQ@163.com>
2019-11-07 22:04:23 +0800

    add version for tensor and storage

--- eleven/src/main.cpp
+++ eleven/src/main.cpp
@@ -8,9 +8,10 @@ using namespace el;
 template<typename Dtype>
 void print10(const Tensor<Dtype>& t) {
 	cout << "=====================" << endl;
-	cout << "shape:  " << t.size() << endl;
+	cout << "shape:  " << t.size() << " | ";
 	cout << "stride: " << t.stride() << endl;
-	cout << "offset: " << t.offset() << endl;
+	cout << "offset: " << t.offset() << " | ";
+	cout << "is unchanged: " << (t.is_unchanged() ? "True":"False") << endl;
 	cout << t << endl;
 }
 
@@ -32,22 +33,18 @@ int main()
 
 	nn::Conv2d<double> conv(3, 3, {2, 2}, {3, 2}, {2, 1});
 	auto ones_tensor = ones({1, 1, 1});
+
 	conv.weight_ = ones_tensor;
 	conv.bias_ = ones_tensor;
 
 	index_t loc[3] = {0, 0, 0};
-	conv.weight_.eval(loc) = 2.;
-	loc[2] = 3;
-	conv.weight_.eval(loc) = 3.;
-	loc[2] = 1;
-	conv.weight_.eval(loc) = 4.;
-	loc[2] = 2;
-	conv.weight_.eval(loc) = 6;
-	loc[2] = 0; loc[1] = 1;
-	conv.bias_.eval(loc) = 2.;
+	conv.weight_[{0, 0, 0}] = 2.;
+	conv.weight_[{0, 0, 3}] = 3.;
+	conv.weight_[{0, 0, 1}] = 4.;
+	conv.weight_[{0, 0, 2}] = 6;
+	conv.bias_[{0, 1, 0}] = 2.;
 
 	auto result = conv.forward(images);
 	print10(result);
-
 	return 0;
 }
--- eleven/src/tensor/storage.h
+++ eleven/src/tensor/storage.h
@@ -8,12 +8,21 @@ namespace el {
 
 template<typename Dtype>
 class Storage {
-    std::shared_ptr<Dtype> bptr_;  // base pointer
+    std::shared_ptr<char> bptr_;  // base pointer
     Dtype* dptr_;  // data pointer
+    void init_version(void) {*reinterpret_cast<index_t*>(bptr_.get()) = 0;}
 public:
     // constructor
-    explicit Storage(index_t dsize):bptr_(new Dtype[dsize]), dptr_(bptr_.get()){}
-    Storage(const Storage& other, index_t offset): bptr_(other.bptr_), dptr_(other.dptr_ + offset){}
+    explicit Storage(index_t dsize)
+        : bptr_(new char[dsize * sizeof(Dtype) + sizeof(index_t)]), 
+          dptr_(reinterpret_cast<Dtype*>(bptr_.get() + sizeof(index_t))) {
+        init_version();
+    }
+    Storage(const Storage& other, index_t offset)
+        : bptr_(other.bptr_), 
+          dptr_(other.dptr_ + offset){
+        init_version();
+    }
     explicit Storage(const Storage& other) = default;
     Storage(const Dtype* data, index_t dsize): Storage(dsize) {
         memcpy(dptr_, data, dsize*sizeof(Dtype));
@@ -21,7 +30,9 @@ public:
     // method
     const Dtype& operator[](index_t i) const {return dptr_[i];}
     Dtype& operator[](index_t i) {return dptr_[i];}
-    index_t offset(void) const {return dptr_ - bptr_.get();}
+    index_t offset(void) const {return dptr_ - (Dtype*)(bptr_.get()) + sizeof(index_t);}
+    index_t version(void) const {return *reinterpret_cast<index_t*>(bptr_.get());}
+    void version_forward(void) const {*reinterpret_cast<index_t*>(bptr_.get()) += 1;}
     // ban
     Storage(void) = delete;
     Storage& operator=(const Storage& other) = delete;
